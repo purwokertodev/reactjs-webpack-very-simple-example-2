1f5d6c68edff30625e8ff51d085e838a75d07afa
wuriyanto-bmd <wuriyanto.wuriyanto.bhinneka.com>
2017-01-09 17:53:34 +0700

    adding timer to header

--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -2,11 +2,34 @@ import React, {Component} from 'react';
 import {Jumbotron} from 'react-bootstrap';
 
 class Header extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      time: new Date()
+    }
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick(){
+    this.setState({
+      time: new Date()
+    });
+  }
+
   render(){
     return (
       <div>
         <Jumbotron>
           <h2>React Bootstrap Demo</h2>
+          <h4>{this.state.time.toLocaleTimeString()}</h4>
         </Jumbotron>
       </div>
     );
